8dc394b2baa83ab4f622e004dce5a9b01dc204ed
Miguel Amores Tamarit <miamta@floridauniversitaria.es>
2016-09-28 08:56:28 +0200

    primer commit

---
+++ miguel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicios Git</title>
+  </head>
+  <body>
+    <?php
+ echo "Miguel";
+     ?>
+     <h1> Primera prueba con el git</h1>
+  </body>
+</html>
